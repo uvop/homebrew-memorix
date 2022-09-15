30e78334f0e670f5be3858a1ca8f61e77454e100
Yuval Saraf <unimonkiez@gmail.com>
2022-09-15 23:49:14 +0300

    Fixed homebrew

--- Formula/memorix.rb
+++ Formula/memorix.rb
@@ -2,14 +2,13 @@ class Memorix < Formula
     version "1.0.0-rc.10"
   
     if Hardware::CPU.intel?
-        url "https://github.com/uvop/memorix/releases/download/v1.0.0-rc.10/memorix-macos-x64.tar.gz"
+        url "https://github.com/uvop/memorix/releases/download/v#{version}/memorix-macos-x64.tar.gz"
         sha256 "143e8bf185e748be1c9384228a42677698cb737452b3d353fd8e06f0a91dd9fa"
     else
-        url "https://github.com/uvop/memorix/releases/download/v1.0.0-rc.10/memorix-macos-x64.tar.gz"
+        url "https://github.com/uvop/memorix/releases/download/v#{version}/memorix-macos-x64.tar.gz"
         sha256 "143e8bf185e748be1c9384228a42677698cb737452b3d353fd8e06f0a91dd9fa"
     end
   
-    name "Memorix"
     desc "Memorix - CLI"
     homepage "https://github.com/uvop/memorix"
   
