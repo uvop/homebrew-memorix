723dcf1d5565f2ff939225a0ee7ffa74ef73fb5a
GitHub Action <action@github.com>
2024-10-29 12:13:19 +0000

    memorix 2.0.3

--- Formula/memorix.rb
+++ Formula/memorix.rb
@@ -1,22 +1,22 @@
 class Memorix < Formula
   desc "Memorix - CLI"
   homepage "https://github.com/uvop/memorix"
-  version "2.0.3-rc1"
+  version "2.0.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/uvop/memorix/releases/download/v#{version}/memorix-macos-arm64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "1123ad5b7005a4a15b5d211be724620084842d1f961455609d78ce9a65074c2c"
     else
       url "https://github.com/uvop/memorix/releases/download/v#{version}/memorix-macos-x64"
-      sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+      sha256 "8f195daff064ab6aea78df6701708630a11dd8d31a22a05d665478c356b86655"
     end
   end
   
   on_linux do
     url "https://github.com/uvop/memorix/releases/download/v#{version}/memorix-linux-x64"
-    sha256 "f0c274ab9f86fb608c94ac774a9caccb416413bfbe1aeb00663b8db95df5fbca"
+    sha256 "4f18944e9ff6f92ad7ba8f6e5c5fc9df332605df81379c2e4c919823a0979b96"
   end
 
   def install
